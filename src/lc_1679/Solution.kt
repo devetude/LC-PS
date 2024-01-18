ef85da1952b7c61e219bf7f015bbe3840c206bce
devetude <devetude@naver.com>
2024-01-18 20:16:12 +0900

    Solve the 1679 problem

---
+++ src/lc_1679/Solution.kt
@@ -0,0 +1,29 @@
+package lc_1679
+
+class Solution {
+    fun maxOperations(nums: IntArray, k: Int): Int {
+        nums.sort()
+        var left = 0
+        var right = nums.lastIndex
+        var count = 0
+        while (left < right) {
+            val sum = nums[left] + nums[right]
+            when {
+                sum < k -> ++left
+                k < sum -> --right
+
+                else -> {
+                    ++count
+                    ++left
+                    --right
+                }
+            }
+        }
+
+        return count
+    }
+}
+
+fun main() {
+    println(Solution().maxOperations(intArrayOf(3, 1, 3, 4, 3), k = 6))
+}
